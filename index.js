b4fe4a96c3b9959a3fa31a492b25833cb763fcd9
Alan <alanitoyah96@gmail.com>
2024-05-12 16:01:21 -0500

    finished function prototype of chrome extension websites can be saved to array and displayed on website in a list format

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-let inputBTN = document.querySelector("#input-btn");
-let inputEl = document.querySelector("#input-el");
-
+let myLeads = [];
+const inputBTN = document.querySelector("#input-btn");
+const inputEl = document.querySelector("#input-el");
+const ulEl = document.querySelector("#ul-el");
 
 inputBTN.addEventListener("click", function() {
-    console.log("Button clicked");
+    myLeads.push(inputEl.value);
+    console.log(myLeads);
+    renderLeads();
 })
+
+function renderLeads(){
+    let listItems = "";
+    for (let i = 0; i < myLeads.length; ++i){
+        listItems += "<li>" + myLeads[i] + "</li>";
+    }
+    ulEl.innerHTML = listItems;
+}
