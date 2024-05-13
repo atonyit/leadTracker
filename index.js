65a38f4cc09fc2a7111aa01b6150aba91aa7b514
Alan <alanitoyah96@gmail.com>
2024-05-13 02:19:53 -0500

    Worked with local storage to get links to persist after refresh, used parse/stringify and then a conditional

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const inputBTN = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if(leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage;
+    renderLeads();
+}
+
 inputBTN.addEventListener("click", function() {
     myLeads.push(inputEl.value);
     inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
     renderLeads();
 })
 
